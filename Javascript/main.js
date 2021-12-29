11d3f0a33f2bb6011fd269a7d9f4c8393619a5e7
AseerUzZaman <aseer@updot.in>
2021-12-30 00:31:15 +0530

    make validate and discard modals

---
+++ Javascript/main.js
@@ -0,0 +1,8 @@
+const openValidateModal = () => {
+  const validateModal = document.querySelector(".validate.modal");
+  validateModal.classList.remove("close");
+};
+const openDiscardModal = () => {
+  const discardModal = document.querySelector(".discard.modal");
+  discardModal.classList.remove("close");
+};
