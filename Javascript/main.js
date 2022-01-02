ce283f4f3d47c281c416933d0d4db81637e7a062
AseerUzZaman <aseer@updot.in>
2022-01-02 20:40:06 +0530

    close page after pressing buttons

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,8 +1,16 @@
 const openValidateModal = () => {
   const validateModal = document.querySelector(".validate.modal");
   validateModal.classList.remove("close");
+
+  setTimeout(() => {
+    window.close();
+  }, 2000);
 };
 const openDiscardModal = () => {
   const discardModal = document.querySelector(".discard.modal");
   discardModal.classList.remove("close");
+
+  setTimeout(() => {
+    window.close();
+  }, 2000);
 };
